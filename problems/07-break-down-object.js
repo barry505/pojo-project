60006b2ae5f2441b1e35ce71db2189e574fac7b7
Barry White <barry505@gmail.com>
2024-01-04 08:54:40 -0600

    First Commit

---
+++ problems/07-break-down-object.js
@@ -0,0 +1,28 @@
+/***********************************************************************
+Write a function `breakDownObj(obj)` that takes in an object as a parameter
+and returns an array containing:  all the keys from the object **and** all the
+values of the object.
+
+**Hint**: Use spread syntax to spread out elements into an array!
+
+
+Examples:
+let object1 = {name: 'Rupert', age: 5, speak: 'Meow'};
+breakDownObj(object1); // => [ 'name', 'age', 'speak', 'Rupert', 5, 'Meow' ]
+
+let object2 = {location: 'NY', borough: 'Brooklyn'};
+breakDownObj(object2); // => [ 'location', 'borough', 'NY', 'Brooklyn' ]
+***********************************************************************/
+
+function breakDownObj(obj) {
+  let keys = [];
+  let values = [];
+  for (let key in obj) {
+    keys.push(key);
+    values.push(obj[key]);
+  }
+  return [...keys, ...values];
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = breakDownObj;
---
+++ problems/14-count-scores.js
@@ -0,0 +1,46 @@
+/***********************************************************************
+Write a function `countScores(people)` that takes in an array of score
+objects (people) as its input. A score object has two key-value pairs:
+the scorer (string) and a point value (number). `countScores(people)` should
+return an object that has key-value pairs listing each person who scored as a key
+and the sum of the total points for the game as their value.
+
+
+Example 1:
+let ppl = [{name: "Anthony", score: 10},
+            {name: "Fred", score : 10},
+            {name: "Anthony", score: -8},
+            {name: "Winnie", score: 12}];
+
+console.log(countScores(ppl)); //=> { Anthony: 2, Fred: 10, Winnie: 12 }
+
+Example 2:
+let peeps = [
+  {name: "Anthony", score: 2},
+  {name: "Winnie", score: 2},
+  {name: "Fred", score: 2},
+  {name: "Winnie", score: 2},
+  {name: "Fred", score: 2},
+  {name: "Anthony", score: 2},
+  {name: "Winnie", score: 2}
+];
+console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
+***********************************************************************/
+
+function countScores(people) {
+  let totals = {};
+  for (let i = 0; i < people.length; i++) {
+    let person = people[i];
+    let name = person.name;
+    let score = person.score;
+    if (totals[name] === undefined) {
+      totals[name] = score;
+    } else {
+      totals[name] += score;
+    }
+  }
+  return totals;
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = countScores;
